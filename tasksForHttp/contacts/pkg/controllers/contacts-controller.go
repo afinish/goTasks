f379d338946b43b59230168e3cfe2886118d9368
heavenmise <notyouraveragehackerman@gmail.com>
2022-01-13 16:37:42 +0500

    Rewrote http-contacts
    Desc: Rewrote http-contacts with cleaner code and better functionality

--- tasksForHttp/contacts/pkg/controllers/contacts-controller.go
+++ tasksForHttp/contacts/pkg/controllers/contacts-controller.go
@@ -1,47 +1,22 @@
 package controllers
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/heavenmise/goTasks/tasksForHttp/contacts/pkg/config"
 	"github.com/heavenmise/goTasks/tasksForHttp/contacts/pkg/models"
 	"github.com/heavenmise/goTasks/tasksForHttp/contacts/pkg/utils"
 )
 
 func GetContacts(c *gin.Context) {
-	fmt.Println("Executing GetContacts function")
-	var Contacts []models.Contact
-	fmt.Println("Panic1")
-	contactRows, err := config.DB.Query(`SELECT * FROM contacts`)
-	fmt.Println("Panic2")
-	if err != nil {
-		fmt.Println("Panic3")
-		panic(err)
-	}
-	
-	defer contactRows.Close()
-
-	for contactRows.Next() {
-		fmt.Println("in contactRows loop")
-		var id int
-		var contact models.Contact
-		err = contactRows.Scan(&id, &contact.FirstName, &contact.LastName, &contact.Phone, &contact.Email, &contact.Position)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(contact)
-		Contacts = append(Contacts, contact)
-	}
+	contacts := models.GetContacts()
 	c.JSON(http.StatusOK, gin.H{
-		"data": Contacts,
+		"data": contacts,
 	})
 }
 
 func GetContact(c *gin.Context) {
 	var id string
-	var Contacts []models.Contact
 	query := c.Request.URL.Query()
 	for key, value := range query {
 		queryValue := value[len(value) - 1]
@@ -49,46 +24,26 @@ func GetContact(c *gin.Context) {
 			id = queryValue
 		}
 	}
-	sqlQuery := fmt.Sprintf(`SELECT * FROM contacts WHERE id=%s`, id)
-
-	contactRows, err := config.DB.Query(sqlQuery)
+	contact, err := models.GetContact(id)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Panic1")
-	defer contactRows.Close()
-	for contactRows.Next() {
-		var id int
-		var firstName, lastName, phone, email, position string
-		err = contactRows.Scan(&id, &firstName, &lastName, &phone, &email, &position)
-		if err != nil {
-			panic(err)
-		}
-		contact := models.Contact{
-			FirstName: firstName, 
-			LastName: lastName, 
-			Phone: phone, 
-			Email: email, 
-			Position: position,
-		}
-		Contacts = append(Contacts, contact)
-	}
 	c.JSON(http.StatusOK, gin.H{
-		"data": Contacts,
+		"data": contact,
 	})
 }
 
 func CreateContact(c *gin.Context) {
-	NewContact := &models.Contact{}
-	utils.ParseBody(c, NewContact)
-	Contact := NewContact.CreateContact()
+	newContact := &models.Contact{}
+	utils.ParseBody(c, newContact)
+	contactId := newContact.CreateContact()
+
 	c.JSON(http.StatusOK, gin.H{
-		"data": Contact,
+		"id": contactId,
 	})
 }
 
 func UpdateContact(c *gin.Context) {
-	var contact models.Contact
 	var updateContact = &models.Contact{}
 	utils.ParseBody(c, updateContact)
 
@@ -100,66 +55,34 @@ func UpdateContact(c *gin.Context) {
 			id = queryValue
 		}
 	}
-	sqlQuery := fmt.Sprintf(`SELECT * FROM contacts WHERE id=%s`, id)
-
-	contactRows, err := config.DB.Query(sqlQuery)
+	
+	models.UpdateContact(id, updateContact)
+	updatedContact, err := models.GetContact(id)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Panic1")
-	defer contactRows.Close()
-	for contactRows.Next() {
-		var id int
-		var firstName, lastName, phone, email, position string
-		err = contactRows.Scan(&id, &firstName, &lastName, &phone, &email, &position)
-		if err != nil {
-			panic(err)
-		}
-		contact = models.Contact{
-			FirstName: firstName, 
-			LastName: lastName, 
-			Phone: phone, 
-			Email: email, 
-			Position: position,
+
+	c.JSON(http.StatusOK, gin.H{
+		"data": updatedContact,
+	})
+}
+
+func DeleteContact(c *gin.Context) {
+	var id string
+	query := c.Request.URL.Query()
+	for key, value := range query {
+		queryValue := value[len(value) - 1]
+		if key == "id" {
+			id = queryValue
 		}
 	}
-	if updateContact.FirstName != "" {
-		contact.FirstName = updateContact.FirstName
-	}
-	if updateContact.LastName != "" {
-		contact.LastName = updateContact.LastName
-	}
-	if updateContact.Phone != "" {
-		contact.Phone = updateContact.Phone
-	}
-	if updateContact.Email != "" {
-		contact.Email = updateContact.Email
-	}
-	if updateContact.Position != "" {
-		contact.Position = updateContact.Position
-	}
-	update := `UPDATE contacts SET first_name=$1, last_name=$2, phone=$3, email=$4, position=$5 WHERE id=$6`
-	_, e := config.DB.Exec(update, contact.FirstName, contact.LastName, contact.Phone, contact.Email, contact.Position, id)
-	if e != nil {
+	models.DeleteContact(id)
+
+	contact, err := models.GetContact(id)
+	if err != nil {
 		panic(err)
 	}
 	c.JSON(http.StatusOK, gin.H{
 		"data": contact,
 	})
-}
-
-func DeleteContact(c *gin.Context) {
-	var name string
-	query := c.Request.URL.Query()
-	for key, value := range query {
-		queryValue := value[len(value) - 1]
-		if key == "first_name" {
-			name = queryValue
-		}
-	}
-	deleteStmt := `DELETE FROM contacts WHERE first_name=$1`
-	_, err := config.DB.Exec(deleteStmt, name)
-	if err != nil {
-		panic(err)
-	}
 }
